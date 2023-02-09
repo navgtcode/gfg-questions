726a583e71428a90f278abc72e79a62268700e70
Navneet Yadav <92593815+Sugaax@users.noreply.github.com>
2023-02-09 12:13:19 +0530

    Update Day 3: Count number of free cell.cpp

--- Day 3: Count number of free cell.cpp
+++ Day 3: Count number of free cell.cpp
@@ -142,4 +142,53 @@ filledRows vector is updated to indicate that this row is now filled, and the ro
 If the column is not filled, the number of remaining free cells is decremented by N - rowCount (since all the rows will now be filled). The 
 filledCols vector is updated to indicate that this column is now filled, and the column count is incremented.
 
-Finally, the number of remaining free cells is stored in the ans vector and this value is returned after all iterations.*/
+Finally, the number of remaining free cells is stored in the ans vector and this value is returned after all iterations.
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+Another Approach
+---------------------------------------------------------------------------------------------------------------------------------------*/
+ 
+
+class Solution{
+  public:
+  vector<long long int> countZero(int n, int k, vector<vector<int>>& arr){
+      vector<long long> ans;
+        int rk = 0, ck = 0;
+        long long temp = n*n;
+        int row[100001] = {0};
+        int col[100001] = {0};
+        for(int i = 0;i<k;i++)
+        {
+            int r = arr[i][0], c = arr[i][1];
+            if(row[r] == 1 && col[c] == 1)
+            {
+                ans.push_back(temp);
+            }
+            else if(row[r] == 1)
+            {
+                col[c] = 1;
+                ck++;
+                temp = temp - n + rk;
+                ans.push_back(temp);
+            }
+            else if(col[c] == 1)
+            {
+                row[r] = 1;
+                rk++;
+                temp = temp - n + ck;
+                ans.push_back(temp);
+            }
+            else
+            {
+                row[r] = 1;
+                col[c] = 1;
+                temp = temp - n + ck - n + rk + 1;
+                ck++;
+                rk++;
+                ans.push_back(temp);
+            }
+        }
+        return ans;
+  }
+};
